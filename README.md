2335f0ee628d88268468bc353761f9e1c75d8e2a
Szymon Sus <sus.szymon3@gmail.com>
2023-04-01 10:53:40 +0200

    feat: add initial boilerplate

---
+++ README.md
@@ -0,0 +1,1 @@
+# 🔎 GitHub Repository Search
---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import { type AppType } from "next/dist/shared/lib/utils";
+
+import "@/styles/globals.css";
+
+const App: AppType = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
